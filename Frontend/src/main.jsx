4f8dea14ee99b66d021918d4957e65377aa00e39
EugeneAndTheMachine <an.nguyen1325@hcmut.edu.vn>
2025-11-22 14:35:20 +0700

    Artist User

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -10,6 +10,8 @@ import SubscriptionPage from '@pages/Subscription/SubscriptionPage'
 import ListenerHomePage from '@pages/Listener/ListenerHomePage'
 import AlbumDetailPage from '@pages/Album/AlbumDetailPage'
 import ArtistProfilePage from '@pages/Artist/ArtistProfilePage'
+import ArtistHomePage from '@pages/Artist/ArtistHomePage'
+import ArtistArtworkDetailPage from '@pages/Artist/ArtistArtworkDetailPage'
 import UserProfilePage from '@pages/Profile/UserProfilePage'
 
 const router = createBrowserRouter([
@@ -44,6 +46,14 @@ const router = createBrowserRouter([
   {
     element: <ArtistProfilePage />,
     path: '/artist/:id'
+  },
+  {
+    element: <ArtistHomePage />,
+    path: '/artist/home'
+  },
+  {
+    element: <ArtistArtworkDetailPage />,
+    path: '/artist/artwork/:id'
   },
   {
     element: <UserProfilePage />,
